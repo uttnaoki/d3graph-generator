e159650344986c36c2eb5ce8e24e4f295b752956
perrier1034 <janne.dc.ib.sa@gmail.com>
2016-07-27 09:46:04 +0900

    rendering in app.post is working

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 app.use(express.static(__dirname + '/public/'));
+app.set('view engine', 'jade');
 var multer = require('multer');
 var storage = multer.diskStorage({
     destination: 'public/csv',
@@ -17,9 +18,10 @@ var routes = {
 app.get('/', routes.index.index);
 
 app.post('/public/csv', upload.single('uploaded_file'), function (req, res) {
-    console.log(req.file)
-
+    console.log(req.file);
+    res.render('bar', { title: 'Express Sample Posted ver 0.0.1', word: req.body.word});
 });
+
 app.listen(3000, function () {
     console.log("port 3000 ...")
 });
--- public/javascripts/file_receiving.js
+++
@@ -1,5 +0,0 @@
-/**
- * Created by perrier on 2016/07/24.
- */
-form(action="...", method="post", enctype="multipart/form-data");
-input(type="file", name="displayImage");
