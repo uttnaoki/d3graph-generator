e9c5fd1b2a54df8bed04d88a346c5bc36fdf5b75
perrier1034 <janne.dc.ib.sa@gmail.com>
2016-08-02 11:05:13 +0900

    add sqlite testing

---
+++ utils/csv_processing.js
@@ -0,0 +1,45 @@
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database(':memory:');
+
+var drop_table_user = function () {
+    db.serialize(function() {
+        db.run('DROP TABLE user');
+    })
+};
+
+var drop_table_csv = function () {
+    db.serialize(function() {
+        db.run('DROP TABLE csv');
+    })
+};
+
+var create_table = function () {
+  db.serialize(function() {
+    db.run('CREATE TABLE user (username TEXT, pass TEXT)');
+    db.run('CREATE TABLE csv (data TEXT, g_type TEXT)');
+  })
+};
+
+var insert_user = function (username, pass) {
+    console.log('INSERT INTO user VALUES (' + username + ',' + pass + ')');
+    // db.serialize(function() {
+    //     db.run('INSERT INTO user VALUES (' + username + ',' + pass + ')');
+    // })
+};
+
+var insert_csv = function (csv, g_type) {
+    db.serialize(function() {
+        db.run('INSERT INTO csv VALUES ' + csv + ',' + g_type + ')');
+    })
+};
+
+var close_db = function () {
+    db.close();
+};
+
+create_table();
+insert_user('foo', 'pass');
+insert_csv('1, 2, 3', 'circle');
+close_db();
+// drop_table_user();
+// drop_table_csv();
