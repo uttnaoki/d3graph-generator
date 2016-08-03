f0ebda9f8bb09675151b2a27667d728765a074d7
perrier1034 <janne.dc.ib.sa@gmail.com>
2016-08-04 08:51:44 +0900

    csv_processing.js is working

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var multer = require('multer');
 var storage = multer.diskStorage({
     destination: 'public/csv',
     filename: function(req, file, cb) {
-        cb(null, file.fieldname + "csv")
+        cb(null, file.fieldname + ".csv")
     }
 });
 var upload = multer({ storage: storage });
--- utils/csv_processing.js
+++ utils/csv_processing.js
@@ -1,5 +1,5 @@
 var sqlite3 = require('sqlite3').verbose();
-var db = new sqlite3.Database(':memory:');
+var db = new sqlite3.Database('test.db');
 
 var drop_table_user = function () {
     db.serialize(function() {
@@ -13,23 +13,27 @@ var drop_table_csv = function () {
     })
 };
 
-var create_table = function () {
+var create_table_user = function () {
+    db.serialize(function() {
+        db.run('CREATE TABLE user (username TEXT, pass TEXT)');
+    })
+};
+
+var create_table_csv = function () {
   db.serialize(function() {
-    db.run('CREATE TABLE user (username TEXT, pass TEXT)');
     db.run('CREATE TABLE csv (data TEXT, g_type TEXT)');
   })
 };
 
 var insert_user = function (username, pass) {
-    console.log('INSERT INTO user VALUES (' + username + ',' + pass + ')');
-    // db.serialize(function() {
-    //     db.run('INSERT INTO user VALUES (' + username + ',' + pass + ')');
-    // })
+    db.serialize(function() {
+        db.run('INSERT INTO user VALUES (?, ?)', username, pass);
+    })
 };
 
 var insert_csv = function (csv, g_type) {
     db.serialize(function() {
-        db.run('INSERT INTO csv VALUES ' + csv + ',' + g_type + ')');
+        db.run('INSERT INTO csv VALUES (?, ?)', csv, g_type);
     })
 };
 
@@ -37,8 +41,8 @@ var close_db = function () {
     db.close();
 };
 
-create_table();
-insert_user('foo', 'pass');
+// create_table_csv();
+insert_user('bar', 'pass');
 insert_csv('1, 2, 3', 'circle');
 close_db();
 // drop_table_user();
